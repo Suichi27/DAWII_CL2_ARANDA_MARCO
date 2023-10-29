5c19bc3cf9f706f850f49a92cdbb8fef714cee93
MarcoAranda <umauricio2705@gmail.com>
2023-10-28 19:23:54 -0500

    Initial commit

---
+++ src/main/java/com/examen/DAWII_CL2_ARANDA_MARCO/DawiiCl2ArandaMarcoApplication.java
@@ -0,0 +1,13 @@
+package com.examen.DAWII_CL2_ARANDA_MARCO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2ArandaMarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2ArandaMarcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/DAWII_CL2_ARANDA_MARCO/DawiiCl2ArandaMarcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.DAWII_CL2_ARANDA_MARCO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2ArandaMarcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
